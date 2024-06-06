a588a9023f10b919a8d119177d142351bb329108
dsdw455 <dsdw455@outlook.com>
2024-06-06 10:33:45 +0800

    Create ekly.js

---
+++ ekly.js
@@ -0,0 +1,29 @@
+.movie-swiper
+{
+height: 90vh;
+}
+.movie-card
+{
+height: 100%;
+width: 100%;
+}
+data: {
+weekMovieList:[
+  {
+    name:"小黄人",
+    comment:"最精彩的电影",
+    imagePath:"/pages/weekly/xhr.jpeg",
+    isHighlyRecommended:false
+  },{
+    name:"泰坦尼克号",
+    comment:"失去的才是永恒的",
+    imagePath:"/pages/weekly/xhr.jpeg",
+    isHighlyRecommended:false
+  },{
+    name:"这个杀手不太冷静",
+    comment:"小萝莉与怪蜀的爱情故事",
+    imagePath:"/pages/weekly/xhr.jpeg",
+    isHighlyRecommended:false
+  }
+]
+  },
